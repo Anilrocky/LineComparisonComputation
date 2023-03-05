ce74195376b810dfc916b240dae6bdb3f5ac7a38
Anilrocky <anilkumarac96@gmail.com>
2023-03-05 12:46:02 +0530

    [Anil] Add:Folder Structure Created

---
+++ LineComparisonComputation/LineComparison.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace LineComparisonComputation
+{
+    public class LineComparison
+    {
+        public void Geometry()
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation");
+        }
+    }
+}
---
+++ LineComparisonComputation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace LineComparisonComputation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LineComparison lineComparison = new LineComparison();
+            lineComparison.Geometry();
+        }
+    }
+}
