66e5efc038ababa578fa9baedf5738e415face1f
Anilrocky <anilkumarac96@gmail.com>
2023-03-06 20:28:52 +0530

    [Anil] Add:Calculate Length of a Line

--- LineComparisonComputation/LineComparison.cs
+++ LineComparisonComputation/LineComparison.cs
@@ -3,9 +3,18 @@ namespace LineComparisonComputation
 {
     public class LineComparison
     {
-        public void Geometry()
+        int x1,x2,y1,y2;
+        public LineComparison(int x1, int x2, int y1, int y2)
         {
-            Console.WriteLine("Welcome to Line Comparison Computation");
+            this.x1 = x1;
+            this.x2 = x2;
+            this.y1 = y1;
+            this.y2 = y2;
+        }
+        public void CalculateLength()
+        {
+            double result = Math.Sqrt(Math.Pow((x2-x1),2) + Math.Pow((y2-y1),2));
+            Console.WriteLine(result);
         }
     }
 }
--- LineComparisonComputation/Program.cs
+++ LineComparisonComputation/Program.cs
@@ -5,8 +5,17 @@ namespace LineComparisonComputation
     {
         static void Main(string[] args)
         {
-            LineComparison lineComparison = new LineComparison();
-            lineComparison.Geometry();
+            Console.WriteLine("Welcome to Line Comparison Computation");
+            Console.WriteLine("Enter x1");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter x2");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter y1");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter y2");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            LineComparison lineComparison = new LineComparison(x1,x2,y1,y2);
+            lineComparison.CalculateLength();
         }
     }
 }
